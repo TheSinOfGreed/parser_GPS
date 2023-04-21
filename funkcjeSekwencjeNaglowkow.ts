6b72e39273ac734aa3cf414ee714b3fef3e440c5
Kamil <kamilandronik1@wp.pl>
2023-04-21 21:58:40 +0200

    [ostatnie poprawki]

--- funkcjeSekwencjeNaglowkow.ts
+++ funkcjeSekwencjeNaglowkow.ts
@@ -1,4 +1,6 @@
-export const GGA = (linia: string) => {
+export const GGA = (linia_old: string) => {
+    const suma = linia_old.substring(linia_old.indexOf('*'));
+    const linia = linia_old.substring(0, linia_old.indexOf('*') );
     const linia_podzielona = linia.split(",");
     
     const naglowek: string = linia_podzielona[0];
@@ -11,7 +13,7 @@ export const GGA = (linia: string) => {
     const NPM: string = linia_podzielona[9] + ", " + linia_podzielona[10];
     const wysokosc_genoid: string = linia_podzielona[11] + ", " + linia_podzielona[12];
     const numer_ID: string = linia_podzielona[13];
-    const suma_kontrolna: string = linia_podzielona[14];
+    
     
     console.log(
         "naglowek: " + naglowek + '\n' +
@@ -25,12 +27,13 @@ export const GGA = (linia: string) => {
         "wysokość w metrach nad poziom morza: " + NPM +'\n' +
         "wysokość geoid (powyżej elipsoidy WGS84): " + wysokosc_genoid +'\n' +
         "numer ID stacji DGPS: " + numer_ID +'\n' +
-
-        "suma kontrolna: " + suma_kontrolna 
+        "suma: " + suma + '\n' 
     );
 };
 
-export const GSA = (linia: string) => {
+export const GSA = (linia_old: string) => {
+    const suma = linia_old.substring(linia_old.indexOf('*'));
+    const linia = linia_old.substring(0, linia_old.indexOf('*') );
     const linia_podzielona = linia.split(",");
 
     const naglowek = linia_podzielona[0];
@@ -40,7 +43,6 @@ export const GSA = (linia: string) => {
     const dop = parseFloat(linia_podzielona[15]);
     const hdop = parseFloat(linia_podzielona[16]);
     const vdop = parseFloat(linia_podzielona[17]);
-    const suma = parseFloat(linia_podzielona[18]);
 
     console.log("Nagłówek: ", naglowek);
     console.log("Sposob okreslenia pozycji: ", tryb + " " + typ_ustawienia_pozycji);
@@ -49,9 +51,12 @@ export const GSA = (linia: string) => {
     console.log("HDOP: ", hdop);
     console.log("VDOP: ", vdop);
     console.log("Suma: ", suma);
+    
 };
 
-export const GSV = (linia: string) => {
+export const GSV = (linia_old: string) => {
+    const suma = linia_old.substring(linia_old.indexOf('*'));
+    const linia = linia_old.substring(0, linia_old.indexOf('*') );
     const linia_podzielona = linia.split(",");
 
     const iden = linia_podzielona[0];
@@ -85,17 +90,20 @@ export const GSV = (linia: string) => {
         "[" + satelity[0].identyfikator_PRN + ", " + satelity[0].wyniesienie_nad_poziom_równika  + ", " + satelity[0].azymut + ", " + satelity[0].SNR + "] " + "\n" +
         "[" + satelity[1].identyfikator_PRN + ", " + satelity[1].wyniesienie_nad_poziom_równika  + ", " + satelity[1].azymut + ", " + satelity[1].SNR + "] " +"\n" +
         "[" + satelity[2].identyfikator_PRN + ", " + satelity[2].wyniesienie_nad_poziom_równika  + ", " + satelity[2].azymut + ", " + satelity[2].SNR + "] " +"\n" +
-        "[" + satelity[3].identyfikator_PRN + ", " + satelity[3].wyniesienie_nad_poziom_równika  + ", " + satelity[3].azymut + ", " + satelity[3].SNR + "] " +"\n"
+        "[" + satelity[3].identyfikator_PRN + ", " + satelity[3].wyniesienie_nad_poziom_równika  + ", " + satelity[3].azymut + ", " + satelity[3].SNR + "] " +"\n" +
+        "suma: " + suma + '\n' 
     );
 };
 
-export const RMC = (linia: string) => {
+export const RMC = (linia_old: string) => {
+    const suma = linia_old.substring(linia_old.indexOf('*'));
+    const linia = linia_old.substring(0, linia_old.indexOf('*') );
     const linia_podzielona = linia.split(",");
     
-    const suma_kontrolna = linia_podzielona.pop();
-    const [aktualnosc_danych, status, latitude, ns, longitude, ew, predkosc, kat, data, odchylenie, magnetic_direction] = linia_podzielona;
+    
+    const [naglowek, aktualnosc_danych, status, latitude, ns, longitude, ew, predkosc, kat, data, odchylenie, magnetic_direction] = linia_podzielona;
 
-    console.log("Nagłówek:", "RMC");
+    console.log("Nagłówek:", naglowek);
     console.log("Aktualność danych:", aktualnosc_danych);
     console.log("Status:", status);
     console.log("Szerokość geograficzna:", latitude, ns);
@@ -104,10 +112,12 @@ export const RMC = (linia: string) => {
     console.log("Kąt śledzenia/poruszania się obiektu:", kat, "stopni");
     console.log("Data:", data);
     console.log("Odchylenie magnetyczne ziemi:", odchylenie, magnetic_direction);
-    console.log("Suma kontrolna:", suma_kontrolna);
+    console.log("Suma: ", suma);
 };
 
-export const GLL = (linia: string) => {
+export const GLL = (linia_old: string) => {
+    const suma = linia_old.substring(linia_old.indexOf('*'));
+    const linia = linia_old.substring(0, linia_old.indexOf('*') );
     const linia_podzielona = linia.split(",");
     const naglowek = linia_podzielona[0];
     const latitude = linia_podzielona[1];
@@ -116,25 +126,28 @@ export const GLL = (linia: string) => {
     const longitude_hemisphere = linia_podzielona[4];
     const czas = linia_podzielona[5];
     const status = linia_podzielona[6];
-    const suma_kontrolna = linia_podzielona[7];
-
+    
     console.log("Nagłówek: ", naglowek);
     console.log("Szerokość geograficzna: ", latitude, latitude_hemisphere);
     console.log("Długość geograficzna: ", longitude, longitude_hemisphere);
     console.log("Czas: ", czas);
     console.log("Status: ", status);
-    console.log("Suma kontrolna: ", suma_kontrolna);
+    console.log("Suma: ", suma);
 };
 
-export const VTG = (linia: string) => {
+export const VTG = (linia_old: string) => {
+    const suma = linia_old.substring(linia_old.indexOf('*'));
+    const linia = linia_old.substring(0, linia_old.indexOf('*') );
     const linia_podzielona = linia.split(",");
     console.log(
+        
         "Naglowek: " + linia_podzielona[0] + '\n' + 
         "Ścieżka poruszania się (w stopniach): " + linia_podzielona[1] + ", " + linia_podzielona[2] + '\n' + 
         "Ścieżka poruszania się na podstawie współrzędnych magnetycznych w stopniach: " + linia_podzielona[3] + ", " + linia_podzielona[4] +  '\n' + 
 
         "Prędkość w węzłach: " + linia_podzielona[5] + ", " + linia_podzielona[6] +  '\n' + 
-        "Prędkość w km/h: " + linia_podzielona[7] + ", " + linia_podzielona[8] +  '\n' + 
-        "suma kontrolna: " + linia_podzielona[0] + '\n' 
+        "Prędkość w km/h: " + linia_podzielona[7] + ", " + linia_podzielona[8] +  '\n'  +
+        "suma: " + suma + '\n' 
+        
     );
 };
