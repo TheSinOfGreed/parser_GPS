3f51640c7c0fec7591c7a6ca9ba33ef275b8fe1d
Kamil <kamilandronik1@wp.pl>
2023-04-30 20:10:46 +0200

    [1# aktualizacja po 1 oddaniu]

--- funkcjeSprawdzajace.ts
+++ funkcjeSprawdzajace.ts
@@ -1,13 +1,53 @@
-//SPRAWDZANIE POPRAWNOSCI LINI
-export const czy_$ = (linia: string, index: number): boolean => {
+//SPRAWDZANIE POPRAWNOSCI LINI //!PRZED ANALIZA LINI
+export const czy_linia_poprawna = (linia: string, index: number): boolean => {
+    if(!czy_$(linia, index)){
+        console.log(index + ". Brak symbolu otwierajacego '$' .\n");
+        return false;
+    }else if(!czy_80_znakow(linia, index)){
+        console.log(index + ". Więcej niż 80 znaków.\n");
+        return false;
+    }else{
+        return true;
+    }
+};
+
+const czy_$ = (linia: string, index: number): boolean => {
     if(linia[0] !== '$') return false;
     else return true;
 };
 
-export const czy_80_znakow = (linia: string, index: number): boolean =>{
+const czy_80_znakow = (linia: string, index: number): boolean =>{
     if(linia.length > 80) return false;
     else return true;
 };
+
+//SPRAWDZANIE POPRAWNOSCI LINI //!PO ANALIZIE LINI
+
+const sprawdz_sume_kontrolna = (sentence: string): string => {
+    let checksum = 0;
+  
+    // Iterujemy po każdym znaku w zdaniu
+    for (let i = 0; i < sentence.length; i++) {
+      const char = sentence.charAt(i);
+  
+      // Jeśli napotkamy znak '*' to kończymy sumowanie i zwracamy obliczoną sumę kontrolną
+      if (char === "*") {
+        return checksum.toString(16);
+      }
+  
+      // Jeśli napotkamy znak '$', ustawiamy sumę kontrolną na 0 i kontynuujemy sumowanie od następnego znaku
+      if (char === "$") {
+        checksum = 0;
+        continue;
+      }
+  
+      // Wykonujemy operację XOR na kodzie ASCII znaku i aktualnej sumie kontrolnej
+      checksum ^= char.charCodeAt(0);
+    }
+  
+    // Jeśli nie ma sumy kontrolnej w zdaniu, zwracamy obliczoną sumę kontrolną
+    return checksum.toString(16);
+  }
 
 
 
--- index.ts
+++ index.ts
@@ -7,18 +7,20 @@ import { wynik } from "./funkcjeSekwencjeNaglowkow";
 
 //funkcje
 import { przeanalizuj_linie } from "./funkcjeAnalizujace";
+import { czy_linia_poprawna } from './funkcjeSprawdzajace';
 
 
-readFile('Dane/zestaw3.txt', 'utf8')
+readFile('Dane/zestaw2.txt', 'utf8')
 .then( daneWejsciowe =>{
     
     //zmienne
     let dane: string[] = daneWejsciowe.split('\n').map( line => line.replace(/\r/g, ''));
 
     dane.forEach((linia, index) => {
-        przeanalizuj_linie(linia, index);
+        if(czy_linia_poprawna(linia, index)) przeanalizuj_linie(linia, index);
     });
     
+    //** WYPISANIE DANYCH
     console.log(wynik);
     wynik.satelity.satelity?.forEach((staelita, index) => {
         console.log("Satelita " + (index+1) + ". [" + staelita.identyfikator_PRN + ", " + staelita.wyniesienie_nad_poziom_równika + ", "+ staelita.azymut + ", "+ staelita.SNR + "]");
