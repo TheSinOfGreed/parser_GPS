b2ee620d15f449220ef73596fc31bde49c03b946
Kamil <kamilandronik1@wp.pl>
2023-06-27 21:07:14 +0200

    [poprawienie wypisywania]

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ import { przeanalizuj_linie } from "./funkcjeAnalizujace";
 import { czy_linia_poprawna } from './funkcjeSprawdzajace';
 
 
-readFile('Dane/new3.txt', 'utf8')
+readFile('Dane/new1.txt', 'utf8')
 .then( daneWejsciowe =>{
     
     //zmienne
@@ -21,9 +21,10 @@ readFile('Dane/new3.txt', 'utf8')
     });
     
     //** WYPISANIE DANYCH
+    if(wynik.satelity.satelity === null) wynik.satelity.czas_ust = null;
     console.log(wynik);
     wynik.satelity.satelity?.forEach((staelita, index) => {
-        console.log("Satelita " + (index+1) + ". [" + staelita.identyfikator_PRN + ", " + staelita.wyniesienie_nad_poziom_równika + ", "+ staelita.azymut + ", "+ staelita.SNR + "]");
+        console.log("Satelita " + (index+1) + ". [" + staelita.identyfikator_PRN + ", " + staelita.wyniesienie_nad_poziom_równika + ", "+ staelita.azymut +  (Number.isNaN(staelita.SNR) ? '' : ", "+staelita.SNR ) + "]");
     });
     
 }).catch(err => {console.log(err)});
